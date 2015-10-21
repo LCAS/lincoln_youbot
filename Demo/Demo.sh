b7671a6357ddfb84770e86317bfee8f86e9c3e4c
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-10-21 13:21:04 +0100

    putting everything into packages

--- Demo/Demo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Starting demo.."
-gnome-terminal -e "bash Gazebo/Start.sh"
-sleep 5
-gnome-terminal -e "bash Moveit/Start.sh"
-sleep 5
-gnome-terminal -e "bash Moveit/Start_Comm.sh"
-echo "Finished."
--- Demo/Moveit/Start_Comm.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Starting Moveit! Commander.."
-cd ~/catkin_ws
-source ./devel/setup.bash
-rosrun moveit_commander moveit_commander_cmdline.py
-echo "Done!"
