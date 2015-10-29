4c9d28b71e87e62e4a9ba8f02dcf4890582a063c
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-10-29 16:08:17 +0000

    Changing tmux script to incorporate keyboard teleop and whycon commands

--- picksie_demo/scripts/start.sh
+++ picksie_demo/scripts/start.sh
@@ -4,25 +4,38 @@ SESSION=$USER
 
 tmux -2 new-session -d -s $SESSION
 # Setup a window for tailing log files
-tmux new-window -t $SESSION:0 -n 'demo_basic'
-tmux new-window -t $SESSION:1 -n 'RViz'
+tmux new-window -t $SESSION:0 -n 'simulation'
+tmux new-window -t $SESSION:1 -n 'moveit!'
+tmux new-window -t $SESSION:2 -n 'keyboard'
+tmux new-window -t $SESSION:3 -n 'whycon'
+tmux new-window -t $SESSION:4 -n 'RViz'
 
 tmux select-window -t $SESSION:0
-tmux split-window -v
-tmux select-pane -t 0
-tmux send-keys "roslaunch picksie_simulation picksie_sim.launch"
-tmux resize-pane -U 30
-
-tmux select-pane -t 1
-tmux send-keys "roslaunch youbot_moveit move_group.launch"
-tmux split-window -v
-
-tmux select-pane -t 2
-tmux send-keys "rosrun moveit_commander moveit_commander_cmdline.py"
-tmux select-pane -t 0
+tmux send-keys "roslaunch picksie_simulation picksie_sim.launch" 
 
 tmux select-window -t $SESSION:1
+tmux send-keys "roslaunch youbot_moveit move_group.launch"
 
+tmux select-window -t $SESSION:2
+tmux split-window -v
+tmux select-pane -t 0
+tmux send-keys "rosrun moveit_commander moveit_commander_cmdline.py"
+
+tmux select-pane -t 1
+tmux send-keys "rosrun teleop_twist_keyboard teleop_twist_keyboard.py"
+
+
+tmux select-window -t $SESSION:3
+tmux split-window -v
+tmux select-pane -t 0
+tmux send-keys "ROS_NAMESPACE=usb_cam rosrun image_proc image_proc"
+
+tmux select-pane -t 1
+tmux send-keys "rosrun whycon whycon _targets:=1 /camera/image_rect_color:=/usb_cam/image_color"
+tmux select-pane -t 0
+
+
+tmux select-window -t $SESSION:4
 tmux send-keys "rosrun rviz rviz"
 
 # Set default window
