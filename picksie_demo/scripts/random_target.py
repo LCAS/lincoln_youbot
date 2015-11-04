6d6b5ff5a3d73d80b7475221a009bdc507e80f40
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-11-04 15:09:43 +0000

    moving files to picksie demo

--- picksie_demo/scripts/random_target.py
+++ picksie_demo/scripts/random_target.py
@@ -1,21 +1,21 @@
 #!/usr/bin/env python
-from simulation import *
+from picksie import *
 
 def Init():
     print "*******************************"
-    print "REQUEST_STATE_INFORMATION - Prints youbot state information to the console"
+    print "RANDOM - Moves youbot to random target"
     print "*******************************"
     #Initialise the youbot object
     youbot = Youbot()
-    #Now print state information
-    # -- Print robot state
-    youbot.Print_State()
-    # -- Print joint state values
+    #Move to random target
     youbot.Print_Joint_Values()
-    #Finally - shutdown the robot
+        #Finally - shutdown the robot
     youbot.Shutdown()
+    
+
 if __name__ == '__main__':
     try:
             Init()
     except rospy.ROSInterruptException:
         pass
+
