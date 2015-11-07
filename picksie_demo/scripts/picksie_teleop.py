2c00572c4f2730f486bb4b40e247010ef31fa5fd
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-11-07 15:21:00 +0000

    XY teleop

--- picksie_demo/scripts/picksie_teleop.py
+++ picksie_demo/scripts/picksie_teleop.py
@@ -8,7 +8,8 @@ class Teleop(object):
     
     def __init__(self, linearAxisIndex = 1, angularAxisIndex = 0):
         rospy.init_node('picksie_teleop')
-
+        self._xymode=False
+        
         self._LinearAxisIndex = rospy.get_param("~linearAxisIndex", linearAxisIndex)
         self._AngularAxisIndex = rospy.get_param("~angularAxisIndex", angularAxisIndex)
         self._LinearScalingFactor = rospy.get_param("~linearScalingFactor", 0.5)
@@ -22,13 +23,23 @@ class Teleop(object):
     
     def _HandleJoystickMessage(self, joyMessage):
         #rospy.logwarn("Handling joystick message: " + str(joyMessage))
-        if joyMessage.buttons[2]:
-            velocityCommand = Twist()
-            velocityCommand.linear.x = self._LinearScalingFactor * joyMessage.axes[self._LinearAxisIndex]
-            velocityCommand.angular.z = self._AngularScalingFactor * joyMessage.axes[self._AngularAxisIndex]    		
+        if joyMessage.buttons[9]:
+            self._xymode = not self._xymode
+        if joyMessage.buttons[5]:
+            if not self._xymode:
+                velocityCommand = Twist()
+                velocityCommand.linear.x = self._LinearScalingFactor * joyMessage.axes[self._LinearAxisIndex]
+                velocityCommand.linear.y = self._LinearScalingFactor * joyMessage.axes[3]
+                velocityCommand.angular.z = self._AngularScalingFactor * joyMessage.axes[self._AngularAxisIndex]    		
+            else:
+                velocityCommand = Twist()
+                velocityCommand.linear.x = self._LinearScalingFactor * joyMessage.axes[self._LinearAxisIndex]
+                velocityCommand.linear.y = self._LinearScalingFactor * joyMessage.axes[self._AngularAxisIndex]
+                velocityCommand.angular.z = self._AngularScalingFactor * joyMessage.axes[3]
         else:
             velocityCommand = Twist()
             velocityCommand.linear.x = 0.0
+            velocityCommand.linear.y = 0.0
             velocityCommand.angular.z = 0.0
         self._VelocityCommandPublisher.publish(velocityCommand)
 
