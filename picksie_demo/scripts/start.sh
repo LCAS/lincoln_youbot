8ec294ea20d79d10bb4ef6c138351e3f6602f6e0
Jaime Pulido Fentanes <pulidofentanes@gmail.com>
2015-11-08 14:33:13 +0000

    adding move_base and usb_cam to start script

--- picksie_demo/scripts/start.sh
+++ picksie_demo/scripts/start.sh
@@ -5,25 +5,28 @@ SESSION=$USER
 tmux -2 new-session -d -s $SESSION
 # Setup a window for tailing log files
 tmux new-window -t $SESSION:0 -n 'robot'
-tmux new-window -t $SESSION:1 -n 'moveit!'
+tmux new-window -t $SESSION:1 -n 'move_base'
 tmux new-window -t $SESSION:2 -n 'joystick'
-tmux new-window -t $SESSION:3 -n 'whycon'
+tmux new-window -t $SESSION:3 -n 'moveit!'
+tmux new-window -t $SESSION:4 -n 'whycon'
 
 
 tmux select-window -t $SESSION:0
 tmux send-keys "roslaunch youbot_driver_ros_interface youbot_driver.launch" 
 
 tmux select-window -t $SESSION:1
-tmux send-keys "roslaunch youbot_moveit move_group.launch"
+tmux send-keys "roslaunch youbot_navigation_local move_base_local.launch"
 
 tmux select-window -t $SESSION:2
 tmux send-keys "roslaunch picksie_demo teleop.launch"
 
-
 tmux select-window -t $SESSION:3
+tmux send-keys "roslaunch youbot_moveit move_group.launch"
+
+tmux select-window -t $SESSION:4
 tmux split-window -v
 tmux select-pane -t 0
-tmux send-keys "ROS_NAMESPACE=usb_cam rosrun image_proc image_proc"
+tmux send-keys "rosrun usb_cam usb_cam_node"
 
 tmux select-pane -t 1
 tmux send-keys "rosrun whycon whycon _targets:=1 _outer_diameter:=0.049 _target_frame:=/camera_frame /camera/image_rect_color:=/usb_cam/image_color"
